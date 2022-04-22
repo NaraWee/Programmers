849b0d35dc082faa1ea64f234ed1c3aea8010f25
NaraWee <79648221+NaraWee@users.noreply.github.com>
2022-04-22 09:08:40 +0900

    예산

---
+++ Level1/Budget.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+
+				/*
+				 * https://programmers.co.kr/learn/courses/30/lessons/12982
+				 */
+
+        int answer = 0;
+
+        Arrays.sort(d);
+
+        for(int i = 0; i < d.length; i++) {
+            if(budget >= d[i]) {
+                budget -= d[i];
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
