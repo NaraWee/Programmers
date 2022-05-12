d31a49a1c05433506e4dfd5a8c12b8ad0566cc08
NaraWee <79648221+NaraWee@users.noreply.github.com>
2022-05-12 10:49:38 +0900

    전화번호 목록

---
+++ Level2/PhoneNumberList.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class PhoneNumberList {
+    public boolean solution(String[] phone_book) {
+        
+        /*
+         * https://programmers.co.kr/learn/courses/30/lessons/42577
+         */
+        
+        boolean answer = true;
+        
+        Arrays.sort(phone_book);
+        
+        for(int i = 0; i < phone_book.length-1; i++) {
+            if(phone_book[i+1].startsWith(phone_book[i])) {
+                return false;
+            }
+        }
+        
+        return answer;
+    }
+}
