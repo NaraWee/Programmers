5b5539a924ff734594d4736601d17aa489315dce
NaraWee <79648221+NaraWee@users.noreply.github.com>
2022-05-13 09:06:22 +0900

    최솟값 만들기

---
+++ Level2/CreateMinimumValue.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class CreateMinimumValue {
+    public int solution(int []A, int []B) {
+        
+        /*
+         * https://programmers.co.kr/learn/courses/30/lessons/12941
+         */
+        
+        int answer = 0;
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+        
+        for(int i = 0; i < A.length; i++) {
+            answer += A[i] * B[B.length-1 - i];
+        }
+
+        return answer;
+    }
+}
